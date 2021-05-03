3c7d691c246501d8fbb6ea01e42168863dff0378
ollie_goodpixel <ollie@goodpixel.co.uk>
2021-05-03 21:26:22 +0100

    Add c&c header example

---
+++ partials/header-cc-brand.php
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <title>Bespoke Hotels</title>
+
+    <meta content="en" name="language">
+    <link rel="stylesheet" href="static/css/main.css" />
+    <link rel="stylesheet" href="static/css/hotels.css" />
+    <link rel="stylesheet" href="static/css/additional.css" />
+    <link rel="stylesheet" href="dist/css/screen.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400i" rel="stylesheet">
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <!-- <script src="dist/js/head.min.js"></script> -->
+
+</head>
+
+<body class="alert-off">
+
+    <div id="header" class="">
+        <div class="desktop">
+
+            <div class="toolbar">
+                <div class="alert-row">
+                    <a href="/pages/covid-19">COVID-19 - How we plan to welcome you back</a>
+                </div>
+                <div class="greybg">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="main-toolbar">
+                                    <div class="padding-15-0 padding-0-0-15">
+                                        <div class="sticky-nav">
+                                            <div class="navigation-icon active">
+                                                <div id="nav-icon-bars" class="open">
+                                                    <span class="icon-bar"></span>
+                                                    <span class="icon-bar"></span>
+                                                    <span class="icon-bar"></span>
+                                                    <div class="sr-only">View Menu</div>
+                                                </div>
+                                            </div>
+                                            <div class="sticky-logo sticky-logo--cc">
+                                                <a href="/">
+                                                    <img src="images/cc-logo-variant.png">
+
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <nav role="navigation" class="bespoke-hotels-menu">
+                                            <ul class="nav">
+                                                <li class="link">
+                                                    <a href="/" title="Bespoke Hotels home"><i
+                                                            class="glyphicon glyphicon-home"></i></a>
+                                                </li>
+
+                                                <li class="link">
+                                                    <a href="https://bespokehotels.com/pages/about_us">Who we are</a>
+                                                </li>
+
+                                                <li class="link">
+                                                    <a href="https://bespokehotels.com/all-hotels">Our portfolio</a>
+                                                </li>
+
+                                                <li class="link">
+                                                    <a href="http://messenger.providesupport.com/messenger/00mkej6pdtb6p1u2tfgtk05t34.html"
+                                                        onclick="window.open(this.href, '', 'resizable=yes,status=yes,location=no,toolbar=no,menubar=no,fullscreen=no,scrollbars=yes,dependent=no,width=645px,left=0px,height=680px,top=0px'); return false;"
+                                                        title="Live help"><i class="glyphicon  bespoke-chat" "=""></i></a>
+                </li>
+            </ul>
+        </nav>
+        <div class=" sticky-nav account-links-container">
+                                                            <div class="header-flex">
+                                                                <a href="#" class="callback">
+                                                                    Request a callback
+                                                                </a>
+                                                                <div>
+                                                                    <div class="cro-number-white">
+                                                                        <p>Central Reservations<br> <span>0844 815
+                                                                                9833</span>
+                                                                        </p>
+                                                                    </div>
+                                                                    <ul class="account-nav" style="display:none;">
+                                                                        <li class="account-not-active">
+                                                                            <a href="#"
+                                                                                class="account-button Businesses"
+                                                                                id="Businesses">Business</a>
+                                                                        </li>
+
+                                                                        <li class="account-not-active">
+                                                                            <a href="#" class="account-button Signin"
+                                                                                id="Signin">Leisure</a>
+                                                                        </li>
+                                                                    </ul>
+                                                                </div>
+                                                            </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="branding">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-4 col-md-3 col-sm-4 col-xs-6">
+                        <div class="brand-burger padding-30-0-0 padding-0-0-20 padding-0-0-15">
+
+                            <div class="navigation-icon active">
+                                <div id="nav-icon-bars-main" class="open">
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <div class="sr-only">View Menu</div>
+                                </div>
+                            </div>
+
+                            <div class="mobile-help-icon-container">
+                                <a href="https://bespokehotels.com/pages/about_us/" class="mobile-help-icon">
+                                    <i class="bespoke-info"></i>
+                                </a>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-5 col-sm-4 col-xs-6">
+                        <div class="padding-20-0-0 padding-0-0-10 bespoke-hotels-logo">
+                            <a href="https://bespokehotels.com/" class="cc-mobile-logo">
+                                <img src="images/cc-logo-variant-colour.png">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-6 account-links-desktop">
+                        <div class="padding-30-0-0 padding-0-0-20 padding-0-15 account-links-container">
+                            <div class="cro-number">
+                                <p>Central Reservations<br> <span>0844 815 9833</span></p>
+                            </div>
+                            <ul class="account-nav" style="display:none;">
+
+                                <li class="account-not-active">
+                                    <a href="#" class="account-button Businesses" id="Businesses">Business</a>
+                                </li>
+
+                                <li class="account-not-active">
+                                    <a href="#" class="account-button Signin" id="Signin">Leisure</a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+    </div>
